f0f13d9a054381252bee345a0818416f39c26534
Christoph Steltner <mail@nplhse.com>
2016-04-16 19:18:38 +0200

    Fixed some issues
    - Added some newlines
    - Added a bunch of comments
    - Added comment blocks at the start of all source files
    - Added a fix for SLInsight that hopefully works…

--- src/AppBundle/Entity/Dummy.php
+++ src/AppBundle/Entity/Dummy.php
@@ -1,5 +1,13 @@
 <?php
 
+
+/*
+ * This file is part of the Arcella package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -63,4 +71,3 @@ class Dummy
         return $this->name;
     }
 }
-
