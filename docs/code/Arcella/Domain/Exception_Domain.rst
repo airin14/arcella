3c942ac288badef286efed9dd83b2ee0fdfa19b0
Christoph Steltner <mail@nplhse.com>
2016-05-22 21:30:21 +0200

    Added a generic Domain Exception

---
+++ docs/code/Arcella/Domain/Exception_Domain.rst
@@ -0,0 +1,8 @@
+================
+Domain Exception
+================
+
+.. phpautomodule::
+   :filename: ../src/Arcella/Domain/Entity/Node.php
+   :members:
+   :undoc-members:
---
+++ src/Arcella/Domain/Exception/DomainException.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Arcella package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Arcella\Domain\Exception;
+
+/**
+ * The Domain Exception is a general exception thrown just by domain objects.
+ *
+ * @package Arcella\Domain\Exception
+ */
+class DomainException extends \Exception
+{
+
+}
