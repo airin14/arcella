a5cd8774e1a124ce7b00e298b8280f520672b09c
Christoph Steltner <mail@nplhse.com>
2016-06-26 15:01:29 +0200

    Fixed a minor issue with the default Arcella package

--- src/Arcella/Domain/NodeInterface.php
+++ src/Arcella/Domain/NodeInterface.php
@@ -7,83 +7,136 @@
  * file that was distributed with this source code.
  */
 
-namespace Arcella\Domain;
+namespace Arcella\Domain\Entity;
+
+use Symfony\Component\PropertyAccess\Exception\NoSuchIndexException;
 
 /**
  * Nodes are the main structure that holds contents within Arcella, they consist of an numeric Id, a title and an array
  * containing the contents, where "text" is the default content. Also there are timestamps from the creation and the
  * last update included.
  *
- * @package Arcella\Domain
+ * @package Arcella\Domain\Entity
  */
-interface NodeInterface
+class Node
 {
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * @var string
+     */
+    protected $slug;
+
+    /**
+     * @var array
+     */
+    protected $fields;
+
+    /**
+     * @var mixed
+     */
+    protected $fieldset;
+
+    /**
+     * @var timestamp
+     */
+    protected $createdAt;
+
+    /**
+     * @var timestamp
+     */
+    protected $updatedAt;
+
+    /**
+     * Node constructor.
+     *
+     * @param $id
+     * @param $title
+     * @param $slug
+     * @param $content
+     * @param $createdAt
+     * @param $updatedAt
+     */
+    private function __construct($id, $title, $slug, $fields, $fielset, $createdAt, $updatedAt)
+    {
+
+    }
+
     /**
      * Returns the Id of the node.
      *
      * @return integer Id of the node.
      */
-    public function getId();
+    public function getId()
+    {
+        return $this->id;
+    }
 
     /**
      * Returns the Title of the node.
      *
      * @return string Title of the node.
      */
-    public function getTitle();
-
-    /**
-     * Updates the Title of the node and returns the changed node with the new title.
-     *
-     * @param string $title The new title for the node.
-     *
-     * @return mixed The node with the new title.
-     */
-    public function setTitle(string $title);
+    public function getTitle()
+    {
+        return $this->title;
+    }
 
     /**
      * Returns the Slug of the node.
      *
      * @return string Slug of the node.
      */
-    public function getSlug();
-
-    /**
-     * Updates the Slug of the node and returns the changed node with the new slug.
-     *
-     * @param string $slug The new slug for the node.
-     *
-     * @return mixed The node with the new slug.
-     */
-    public function setSlug(string $slug);
+    public function getSlug()
+    {
+        return $this->slug;
+    }
 
     /**
      * Returns the Content of the node.
      *
+     * @param string $key
+     *
      * @return array All the contents inside a associated array, with "text" as default.
      */
-    public function getContent();
+    public function getFields($key = null)
+    {
+        if (isset($key)) {
+            if (!array_key_exists($key, $this->content)) {
+                throw new NoSuchIndexException('No such $key in $this->content');
+            }
 
-    /**
-     * Sets Content of the node.
-     *
-     * @param array $content The new content for the node.
-     *
-     * @return mixed The node with the new content.
-     */
-    public function setConcent(array $content);
+            return $this->content[$key];
+        }
+
+        return $this->content;
+    }
 
     /**
      * Returns the timestamp of the creation of the node.
      *
      * @return integer Timestamp of the nodes creation.
      */
-    public function getCreated();
+    public function getCreated()
+    {
+        return $this->createdAt;
+    }
 
     /**
      * Returns the timestamp of the last update of the node.
      *
      * @return integer Timestamp of the nodes last update.
      */
-    public function getUpdated();
+    public function getUpdated()
+    {
+        return $this->updatedAt;
+    }
 }
