d61de735475d8437a22861589efe465f5a447aa3
Christoph Steltner <mail@nplhse.com>
2016-06-29 17:38:41 +0200

    Aaahhh... Why don't I double-check before committing...
    Fixed some small issues that I’ve been to dumb to discover.

--- src/AppBundle/DataFixtures/ORM/LoadFixtures.php
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of the Arcella package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AppBundle\DataFixtures\ORM;
 
 use AppBundle\Entity\Dummy;
@@ -7,8 +14,15 @@ use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Nelmio\Alice\Fixtures;
 
+/**
+ * Class LoadFixtures
+ * @package AppBundle\DataFixtures\ORM
+ */
 class LoadFixtures implements FixtureInterface
 {
+    /**
+     * @param ObjectManager $manager
+     */
     public function load(ObjectManager $manager)
     {
         $objects = Fixtures::load(__DIR__.'/dummy.yml', $manager);
