e3d1b67679c8ca09ba11a6cd88f838434b30247e
Christoph Steltner <mail@nplhse.com>
2016-09-16 00:21:38 +0200

    Began to implement a mailer for email validation

---
+++ src/Arcella/UserBundle/EventListener/UserEmailValidationListener.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Arcella package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Arcella\UserBundle\EventListener;
+
+use Arcella\Domain\Event\UserUpdatedEmailEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class UserEmailValidationListener
+{
+    protected $twig;
+    protected $mailer;
+
+    public function __construct(\Twig_Environment $twig, \Swift_Mailer $mailer)
+    {
+        $this->twig = $twig;
+        $this->mailer = $mailer;
+    }
+
+    public function onUserUpdatedEmail(UserUpdatedEmailEvent $event)
+    {
+        $user = $event->getUser();
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Hello Email')
+            ->setFrom('send@example.com')
+            ->setTo('recipient@example.com')
+            ->setBody(
+                $this->twig->render(
+                    'Emails/email_validation.html.twig',
+                    array('name' => $user->getUsername())
+                ),
+                'text/html'2
+            );
+
+        $this->mailer->send($message);
+    }
+}
