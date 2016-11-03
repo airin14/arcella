0f778d993dab787ae322a98087b08baec44dd503
Christoph Steltner <mail@nplhse.com>
2016-11-03 03:31:44 +0100

    Of cause I must have forgotten something...

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new SimpleBus\SymfonyBridge\SimpleBusCommandBusBundle(),
             new Arcella\UserBundle\ArcellaUserBundle(),
             new Arcella\UtilityBundle\ArcellaUtilityBundle(),
